7f5b6c9c640eb87fb63d57c28553eeb2e9f449af
Mark Creamer <bubzor888@gmail.com>
2018-10-04 12:02:45 -0500

    pcc deploy final

--- src/dialogs/pccDeployDialog.js
+++ src/dialogs/pccDeployDialog.js
@@ -1,19 +1,6 @@
-// import ServiceNow from '../clients/serviceNowClient';
-
-// const data = {
-//   short_description: 'Deploy static content',
-//   description: 'Deploy content from 19.2 to Prod',
-//   urgency: '1',
-//   priority: '3',
-//   assignment_group: 'Software',
-//   category: 'Software',
-// };
-
-// ServiceNow.createNewTask(data, 'change_request', res => {
-//   console.log(res);
-// });
-
 import { WaterfallDialog } from 'botbuilder-dialogs';
+import ServiceNow from '../clients/serviceNowClient';
+
 import { getEntity } from '../entityUtils';
 
 export const INTENT = 'Content Deployment';
@@ -30,13 +17,16 @@ export function dialog(prompt, luisState) {
       return step.prompt(prompt, 'What branch do you want to deploy?');
     }
 
+    console.log(`found luis entity: ${branch}`);
     step.values[ENTITY_BRANCH] = branch;
-    return step.next(2);
+    return step.next();
   }
 
   async function branchCapture(step) {
-    step.values[ENTITY_BRANCH] = step.result;
-    console.log(`set ${step.result}`);
+    if (step.values[ENTITY_BRANCH] === undefined) {
+      step.values[ENTITY_BRANCH] = step.result;
+      console.log(`seting branch to ${step.result}`);
+    }
     return step.next();
   }
 
@@ -46,14 +36,17 @@ export function dialog(prompt, luisState) {
     if (environment === undefined) {
       return step.prompt(prompt, 'Where do you want to deploy?');
     }
-    step.values[ENTITY_ENVIRONMENT];
+    console.log(`found luis entity: ${environment}`);
+    step.values[ENTITY_ENVIRONMENT] = environment;
 
-    return step.next(2);
+    return step.next();
   }
 
   async function environmentCapture(step) {
-    step.values[ENTITY_ENVIRONMENT] = step.result;
-    console.log(`set ${step.result}`);
+    if (step.values[ENTITY_ENVIRONMENT] === undefined) {
+      step.values[ENTITY_ENVIRONMENT] = step.result;
+      console.log(`seting environment to ${step.result}`);
+    }
     return step.next();
   }
 
@@ -61,10 +54,31 @@ export function dialog(prompt, luisState) {
     const environment = step.values[ENTITY_ENVIRONMENT];
     const branch = step.values[ENTITY_BRANCH];
 
-    await step.context.sendActivity(
-      `Ok, I'll deploy the ${branch} branch to ${environment}`,
+    const number = await createNewTask(branch, environment);
+
+    step.context.sendActivity(
+      `Ok, I've opened up a ticket ${number} to deploy the ${branch} branch to ${environment}`,
     );
+    // console.log(res)
+    // ;
     return step.endDialog();
+  }
+
+  async function createNewTask(branch, environment) {
+    const data = {
+      short_description: 'Deploy static content',
+      description: `Deploy the ${branch} to ${environment} environment`,
+      urgency: '1',
+      priority: '3',
+      assignment_group: 'Software',
+      category: 'Software',
+    };
+
+    return new Promise(resolve => {
+      ServiceNow.createNewTask(data, 'change_request', res => {
+        resolve(res.number);
+      });
+    });
   }
 
   return new WaterfallDialog(INTENT, [
