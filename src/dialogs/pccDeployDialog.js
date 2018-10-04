9d0b0164946ee0b3d3ed85cfbda80f0261fc720d
Steven Goff <steveng9@gmail.com>
2018-10-04 10:25:35 -0500

    Updates

--- src/dialogs/pccDeployDialog.js
+++ src/dialogs/pccDeployDialog.js
@@ -10,19 +10,31 @@ const ENTITY_ENVIRONMENT = 'environment';
 export function dialog(prompt, luisState) {
   async function step1(step) {
     const result = await luisState.get(step.context, {});
-    step.context.sendActivity(`Entities: ${result}`);
+
+    const branch = getEntity(result, 'branch');
+
+    step.context.sendActivity(`I see you want to push to ${branch}`);
+    step.context.sendActivity(`Entities: ${JSON.stringify(result)}`);
+    step.next(2);
+  }
+
+  async function branchStep(step) {
     return step.prompt(prompt, 'What branch do you want to deploy?');
   }
 
-  async function step2(step) {
-    // access user input from previous step
-    step.values[BRANCH] = step.result;
-
-    // send a message to the user
-    await step.prompt(prompt, 'Where do you want to deploy?');
+  async function destinationStep(step) {
+    return step.prompt(prompt, 'Where do you want to deploy?');
   }
 
-  async function step3(step) {
+  // async function step2(step) {
+  //   // access user input from previous step
+  //   step.values[BRANCH] = step.result;
+
+  //   // send a message to the user
+  //   await step.prompt(prompt, 'Where do you want to deploy?');
+  // }
+
+  async function dialogCompleteStep(step) {
     await step.context.sendActivity(
       `Ok, I'll deploy the ${step.values[BRANCH]} branch to ${step.result}`,
     );
@@ -32,10 +44,15 @@ export function dialog(prompt, luisState) {
   function getEntity(list, type) {
     const result = _.find(list, item => item.type === type);
     if (result) {
-      return result.value;
+      return result.entity;
     }
     return undefined;
   }
 
-  return new WaterfallDialog(INTENT, [step1, step2, step3]);
+  return new WaterfallDialog(INTENT, [
+    step1,
+    branchStep,
+    destinationStep,
+    dialogCompleteStep,
+  ]);
 }
