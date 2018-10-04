2297145b33d8ea88f901d29f6e5b8d849798047c
Steven Goff <steveng9@gmail.com>
2018-10-04 08:52:51 -0500

    Real messages

--- src/dialogs/helpDialog.js
+++ src/dialogs/helpDialog.js
@@ -6,36 +6,16 @@ export const INTENT = 'HELP';
 const getChoices = () => {
   const cardOptions = [
     {
-      value: 'Animation Card',
-      synonyms: ['1', 'animation', 'animation card'],
+      value: 'Get my PRs',
     },
     {
-      value: 'Audio Card',
-      synonyms: ['2', 'audio', 'audio card'],
+      value: 'Show me my issues',
     },
     {
-      value: 'Hero Card',
-      synonyms: ['3', 'hero', 'hero card'],
+      value: 'Deploy content to PA',
     },
     {
-      value: 'Receipt Card',
-      synonyms: ['4', 'receipt', 'receipt card'],
-    },
-    {
-      value: 'Signin Card',
-      synonyms: ['5', 'signin', 'signin card'],
-    },
-    {
-      value: 'Thumbnail Card',
-      synonyms: ['6', 'thumbnail', 'thumbnail card'],
-    },
-    {
-      value: 'Video Card',
-      synonyms: ['7', 'video', 'video card'],
-    },
-    {
-      value: 'All Cards',
-      synonyms: ['8', 'all', 'all cards'],
+      value: 'Show me my stories',
     },
   ];
 
@@ -44,13 +24,6 @@ const getChoices = () => {
 
 export function dialog(prompt) {
   async function step1(step) {
-    // access user input from previous step
-    // send a message to the user
-    //   await step.context.sendActivity(`You mean you don't have all the answers?`);
-
-    // Prompt the user with the configured PromptOptions.
-    // Create the PromptOptions which contain the prompt and reprompt messages.
-    // PromptOptions also contains the list of choices available to the user.
     const promptOptions = {
       prompt: 'Please select a card:',
       reprompt:
