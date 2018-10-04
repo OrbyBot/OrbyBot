9c87bba7bf2230c9e4e74463dabe734e4f5cae36
Steven Goff <steveng9@gmail.com>
2018-10-04 11:00:06 -0500

    Fix lint

--- src/clients/serviceNowClient.js
+++ src/clients/serviceNowClient.js
@@ -1,15 +1,11 @@
-const sn = require('servicenow-rest-api');
+const ServiceNow = require('servicenow-rest-api');
 
-const ServiceNow = new sn(
+const serviceNow = new ServiceNow(
   process.env.SN_DOMAIN,
   process.env.SN_USER,
   process.env.SN_PASSWORD,
 );
 
-ServiceNow.Authenticate();
+serviceNow.Authenticate();
 
-export default ServiceNow;
-// ServiceNow.getSampleData('change_request', res => {
-//   //
-//   console.log(res);
-// });
+export default serviceNow;
