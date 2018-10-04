e0e47008e3c1368ab14a09f727befa5c1bd3ccad
Mark Creamer <bubzor888@gmail.com>
2018-10-04 08:44:07 -0500

    content dialog

--- src/dialogs/pccDeployDialog.js
+++ src/dialogs/pccDeployDialog.js
@@ -1,1 +1,22 @@
-// @todo
+import { WaterfallDialog } from 'botbuilder-dialogs';
+
+export const INTENT = 'Content Deployment';
+
+export function dialog(prompt) {
+  async function step1(step) {
+    return step.prompt(prompt, 'What is your name?');
+  }
+
+  async function step2(step) {
+    // access user input from previous step
+    const lastStepAnswer = step.result;
+    console.log('Last answer: ', lastStepAnswer);
+
+    // send a message to the user
+    await step.context.sendActivity(`Thanks ${lastStepAnswer}`);
+
+    // OR end
+    return step.endDialog();
+  }
+  return new WaterfallDialog(INTENT, [step1, step2]);
+}
